26590e07685d7bda3926e0252cdd9934ecc11251
kyan.daigneault <kyan.daigneault@pixsenses.com>
2021-01-14 13:05:48 -0500

    Animation du texte orange de l'article manifeste (intégration des span aux endroits manquants).

--- js/script-article.js
+++ js/script-article.js
@@ -5,6 +5,8 @@ if (window.innerWidth > 1000) {
         var windowBottom = $(this).scrollTop() + $(this).innerHeight();
         var orangeIcon = $("img.img-anim-scroll");
         var soleil = $("div.text-soleil");
+        var textManifesteLeft = $("section.texte-manifeste span");
+        var textManifesteRight = $("section.texte-manifeste em");
         var screenHeight = $(this).innerHeight();
 
         $("#orange-icon").each(function() {
@@ -14,7 +16,6 @@ if (window.innerWidth > 1000) {
             if ((objectTop < windowBottom) && (objectBottom > windowBottom-screenHeight)) {
                 orangeIcon.eq(0).css('right',(50-(windowBottom-objectTop)/10)+'vw');
                 orangeIcon.eq(1).css('left',(50-(windowBottom-objectTop)/10)+'vw');
-                console.log();
                 if ((50-(windowBottom-objectTop)/10) < 0){
                     orangeIcon.eq(0).css('right','0vw');
                     orangeIcon.eq(1).css('left','0vw');
@@ -27,7 +28,6 @@ if (window.innerWidth > 1000) {
             var soleilWidth = soleil.css('width');
             
             if ((objectTop < windowBottom) && (objectBottom > windowBottom-screenHeight)) {
-                console.log((windowBottom-objectTop)/10);
                 soleil.eq(0).css('left',(((windowBottom-objectTop)/10)-10)+'vw');
                 soleil.eq(1).css('left',(((windowBottom-objectTop)/10)-15)+'vw');
                 soleil.eq(2).css('left',(((windowBottom-objectTop)/10)-20)+'vw');
@@ -43,6 +43,20 @@ if (window.innerWidth > 1000) {
                 }
                 if ((((windowBottom-objectTop)/10)-25)>87.5){
                     soleil.eq(3).css('left','87.5vw');
+                }
+            }
+        });
+        $("#orange-text").each(function() {
+            var objectTop = $(this).offset().top;
+            var objectBottom = $(this).offset().top + $(this).outerHeight()*1;
+            
+            if ((objectTop < windowBottom) && (objectBottom > windowBottom-screenHeight)) {                
+                textManifesteLeft.css('left',(-75+(windowBottom-objectTop)/10)+'vw');
+                textManifesteRight.css('left',(75-(windowBottom-objectTop)/10)+'vw');
+                console.log("allo");
+                if ((75-(windowBottom-objectTop)/10) < 0){
+                    textManifesteLeft.css('left','0vw');
+                    textManifesteRight.css('left','0vw');
                 }
             }
         });
